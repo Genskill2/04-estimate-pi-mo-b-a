b1abe7a7f62022b4882e8c8f621917e495cc5826
mo-b-a <84846038+mo-b-a@users.noreply.github.com>
2021-06-02 23:22:06 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -3,30 +3,10 @@
 #include <stdio.h>
 #include <math.h>
 
-float wallis_pi(int);
-
-int main(void) {
-  float pi;
-  for (int i=1; i<5; i++) {
-    pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) > 0.15)) {
-      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
-      //abort();
-    }
-  }
-
-  for (int i=500; i<3000; i++) {
-    pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) < 0.01)) {
-      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
-      //abort();
-    }
-  }
-}
-
 float wallis_pi(int n)
 {
-  float x,pi,y=1;
+  float x,pi;
+  float y=1;
   for(int i=1;i<n;i++)
   {
     x=(4*i*i);
@@ -35,3 +15,29 @@ float wallis_pi(int n)
  pi=2*y;
   return pi;
 }
+
+
+
+int main(void) {
+  float pi;
+  for (int i=1; i<5; i++)
+  {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) > 0.15))
+    {
+      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
+      abort();
+    }
+  }
+
+  for (int i=500; i<3000; i++)
+  {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) < 0.01))
+    {
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
+      abort();
+    }
+  }
+}
+
